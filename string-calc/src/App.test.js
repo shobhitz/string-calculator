8721aa0d6586c0e7f442c650f0fc6b08acdaf32a
Shobhit <shobhitgoel1990@gmail.com>
2025-03-17 08:17:21 +0530

    initial test cases

--- string-calc/src/App.test.js
+++ string-calc/src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("String Calculator", () => {
+  test('Validate Query Input', () => {
+    render(<App />);
+    const queryElem = screen.getByLabelText('Query')
+    expect(queryElem).toBeInTheDocument();
+  });
+  
+  test('Validate Result Box is present', () => {
+    render(<App />);
+    expect(screen.getByLabelText('Result')).toBeInTheDocument()
+  });
+  
+  test('Check if button is present', () => {
+    render(<App />);
+    const submitButton = screen.getByRole('button', {name: /calculate/i});
+    expect(submitButton).toBeInTheDocument();
+  });
+})
+
