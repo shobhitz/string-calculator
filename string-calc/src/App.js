f8d5169019af5ebbedfe1fc409de52eb6f5ee3a8
Shobhit <shobhitgoel1990@gmail.com>
2025-03-17 09:03:19 +0530

    added functionality to add basic operations

--- string-calc/src/App.js
+++ string-calc/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
+  const calc = () => {
+    const nums = input.split(',')
+    const len = nums.length
+    let sum = 0
+    
+    for(let i =0;i<len;i++){
+      const num = Number(nums[i])
+      if(num >= 0){
+        sum += num
+      }
+    }
+    setOutput(sum)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label id="query-label">Query</label>
+      <input aria-labelledby="query-label" value={input} onChange={(e) => setInput(e.target.value)} />
+      <button type="submit" name="calculate" onClick={calc}>Calculate</button>
+
+      <label id="result">Result</label>
+      <input aria-labelledby="result" value={output} readOnly/>
     </div>
   );
 }
