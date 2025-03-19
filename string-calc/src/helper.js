40d4d2a42c00b9c88b07a44a6ae7542bb5b33f38
Shobhit <shobhitgoel1990@gmail.com>
2025-03-19 07:19:23 +0530

    adding functionality to detect custom

--- string-calc/src/helper.js
+++ string-calc/src/helper.js
@@ -3,8 +3,38 @@ const checkNegative = (num) => {
   return num < 0
 }
 
+const getStringBetween = (str, initialString, endString) => {
+    return str.substr(initialString.length, str.indexOf(endString) - initialString.length);
+}
+
+const getStringFrom = (str, char) =>{
+  return str.substr(str.indexOf(char)+2)
+  
+}
+
+const hasHeader = (string) => {
+  return string.startsWith("//")
+}
+
+const constructSeparator = (custom) => {
+  const defaultDelimiter = new RegExp("[s,]+");
+  if(custom.length > 0){
+    const regex = new RegExp("[s," + custom + "]");
+    return regex
+  }
+  return defaultDelimiter
+}
+
+const splitLines = (str) =>{
+  const arr = str.split(/\r?\\n/);
+  return arr.join()
+} 
+
 export const calculate = (str) => {
-  const nums = str.split(/[\s,\\n]+/)
+  const custom = hasHeader(str) ? getStringBetween(str, '//', '\\n') : ""
+  let input = hasHeader(str) ? getStringFrom(str, '\\n') : str
+  input = splitLines(input)
+  const nums = input.split(constructSeparator(custom))
 
   const len = nums.length
   let sum = 0
@@ -20,6 +50,5 @@ export const calculate = (str) => {
       sum += num
     }
   }
-
   return sum 
 }
